82941d96b1e6171b62877b4426c21e91a186144a
gabriel <gabriel.stocco06@gmail.com>
2025-03-11 17:53:16 -0300

    Att component rick&morty

--- src/app/components/api-rick-and-morty/api-rick-and-morty.component.ts
+++ src/app/components/api-rick-and-morty/api-rick-and-morty.component.ts
@@ -1,24 +1,27 @@
 import { Component } from '@angular/core';
 import { ApiServiceService } from '../../services/api-service.service';
-import { PaginationComponent } from './pagination/pagination.component';
 
 @Component({
   selector: 'app-api-rick-and-morty',
-  imports: [PaginationComponent],
+  imports: [],
   templateUrl: './api-rick-and-morty.component.html',
   styleUrl: './api-rick-and-morty.component.css',
 })
 export class ApiRickAndMortyComponent {
   constructor(private apiCard: ApiServiceService) {}
-
   characters: Array<any> = [];
 
-  ngOnInit(): void {
-    this.getCard();
+  ngOnInit() {
+    this.getCard(1);
   }
+
   pagNum: number = 1;
 
-  passPage(pag: number = 1) {
+  passPageEmit(pag: number) {
+    this.getCard(pag + this.pagNum);
+  }
+
+  passPage(pag: number) {
     if (pag === 0) {
       this.pagNum = pag + 1;
     } else if (pag == 42) {
@@ -29,9 +32,11 @@ export class ApiRickAndMortyComponent {
     this.getCard(this.pagNum);
   }
 
-  getCard(pag: number = 1) {
+  getCard(pag: number) {
     this.characters.splice(0, this.characters.length);
-    this.apiCard.getCardService(pag).subscribe((result) => {
+    this.pagNum = pag;
+    console.log(this.pagNum);
+    return this.apiCard.getCardService(pag).subscribe((result) => {
       this.characters = result.results;
     });
   }
